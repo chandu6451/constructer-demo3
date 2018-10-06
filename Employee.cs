bf3157de2048c243f193f4b68e9b4a6ec5d8fdf9
chandu6451 <43878555+chandu6451@users.noreply.github.com>
2018-10-06 12:37:38 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo3
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        double salary;
+
+        public int EmployeeID { get { return id; } }
+        public string EmployeeName { get { return name; } }
+        public double Salary { get { return salary; } }
+
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDemo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(101, "Robert", 30000);
+
+            Console.WriteLine("Employee ID : " + emp.EmployeeID);
+            Console.WriteLine("Employee Name : " + emp.EmployeeName);
+            Console.WriteLine("Employee Salary : " + emp.Salary);
+
+            Console.ReadKey();
+        }
+    }
+}
